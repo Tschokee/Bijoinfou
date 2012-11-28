d2da672e5e69b3fb7c81f289fd060b4622b401b7
Alterion <hevnerjaons@hotmail.com>
2012-11-28 18:05:56 +0100

    implemented algorithm
    elvileg jól működik az algoritmus, már csak egy kezelőfelületet kellene
    csinálni. kell 2 mező a két fájlnévnek, egy a küszöb érték megadásához.
    a program.cs ben megtalálod ezek helyét. szerintem még egy start gomb is
    kéne az egész elindításához

---
+++ FusionGene/FusionGene/Algoritmus.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Juri
+ * Date: 2012.11.27.
+ * Time: 15:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using FusionGene;
+using FusionGeneDatabase;
+
+namespace FusionGene
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public static class Algoritmus
+	{
+		
+		public static bool algorithm(string s1, string s2, string f, int h)
+		{
+			int i, j, k, kuszob1 = 0, kuszob2 = 0, kezdo1 = -1, kezdo2 = -1;
+			if( (s1.Length + s2.Length) > f.Length )
+				return false;
+			for( i = 0 ; i < f.Length - s1.Length ; i++)
+			{
+				k = 0;
+				kuszob1 = h;
+				for( j = i ; j < s1.Length + i ; j++, k++ )
+				{
+					if( !f[j].Equals(s1[k]) )
+						kuszob1--;
+					if( kuszob1 < 0 )
+						break;
+					if( j + 1 == s1.Length)
+						kezdo1 = i;
+				}
+				if( kezdo1 != -1 )
+					break;
+			}
+			for( i = 0 ; i < f.Length - s2.Length ; i++)
+			{
+				k = 0;
+				kuszob2 = h;
+				for( j = i ; j < s2.Length + i ; j++, k++ )
+				{
+					if( !f[j].Equals(s2[k]) )
+						kuszob2--;
+					if( kuszob2 < 0 )
+						break;
+					if( j + 1 == s2.Length)
+						kezdo2 = i;
+				}
+				if( kezdo2 != -1 )
+					break;
+			}
+			if( kezdo1 == -1 || kezdo2 == -1 )
+				return false;
+			if( Math.Abs(( kezdo1 + s1.Length ) - kezdo2) - ( kuszob1 + kuszob2) > h)
+				return false;
+			if( Math.Abs(( kezdo2 + s2.Length ) - kezdo1) - ( kuszob1 + kuszob2) > h)
+				return false;
+			return true;
+		}
+		
+	}
+}
---
+++ FusionGene/FusionGene/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Juri
+ * Date: 2012.11.27.
+ * Time: 15:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+using FusionGene;
+using FusionGeneDatabase;
+using System.IO;
+
+namespace FusionGene
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			StreamWriter writer = new StreamWriter("Output.txt");
+			GeneDatabase g1 = new GeneDatabase("ACLA.fasta");
+			GeneDatabase g2 = new GeneDatabase("AGOS1.fasta");
+			for(int i = 0 ; i < g1.count() - 1 ; i++)
+			{
+				for(int j = i + 1 ; j < g1.count() ; j++)
+				{
+					for(int k = 0 ; k < g2.count() ; k++)
+					{
+						if(Algoritmus.algorithm(g1.getGene(i).getGeneString(), g1.getGene(j).getGeneString(), g2.getGene(k).getGeneString(), 200))
+						{
+							writer.WriteLine(g2.getGene(k).getId() + " : " + g1.getGene(i).getId() + " , " + g1.getGene(j).getId());
+						}
+					}
+				}
+			}
+//			Application.EnableVisualStyles();
+//			Application.SetCompatibleTextRenderingDefault(false);
+//			Application.Run(new MainForm());
+			//Algoritmus.algorithm();
+			writer.Close();
+		}
+		
+	}
+}
